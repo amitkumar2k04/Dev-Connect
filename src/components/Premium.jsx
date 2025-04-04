3f7f2b9ae91b21583e47348defed6f89421a6448
amitkumar2k04 <amitkumar2k00@gmail.com>
2025-04-04 14:41:55 +0530

    Added verifyPremiumUser & handledBuyClick

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,12 +1,27 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constaints";
+import { useEffect, useState } from "react";
 
-const premium = () => {
+const Premium = () => {
+  const [isUserPremium, setIsUserPremium] = useState(false);
+  useEffect(() => {
+    verifyPremiumUser();
+  }, []);
+
+  const verifyPremiumUser = async () => {
+    const res = await axios.get(BASE_URL + "/premium/verify", {
+      withCredentials: true,
+    });
+    if (res.data.isPremium) {
+      setIsUserPremium(true);
+    }
+  };
+
   const handledBuyClick = async (type) => {
     const order = await axios.post(
       BASE_URL + "/payment/create",
       {
-        memberShipType : type,
+        memberShipType: type,
       },
       { withCredentials: true }
     );
@@ -15,27 +30,30 @@ const premium = () => {
     const { amount, keyId, currency, notes, orderId } = order.data;
 
     const options = {
-        key: keyId,
-        amount,
-        currency,
-        name: 'DevTinder',
-        description: 'Connect to Other Developers',
-        order_id: orderId,
-        prefill: {
-          name: notes.firstName + " " + notes.lastName,
-          email: notes.emailId,
-          contact: '9999999999'
-        },
-        theme: {
-          color: '#F37254'
-        },
-      };
+      key: keyId,
+      amount,
+      currency,
+      name: "DevTinder",
+      description: "Connect to Other Developers",
+      order_id: orderId,
+      prefill: {
+        name: notes.firstName + " " + notes.lastName,
+        email: notes.emailId,
+        contact: "9999999999",
+      },
+      theme: {
+        color: "#F37254",
+      },
+      handler: verifyPremiumUser,
+    };
 
     const rzp = new window.Razorpay(options);
     rzp.open();
   };
 
-  return (
+  return isUserPremium ? (
+    "You're already a premium user"
+  ) : (
     <div className="m-10">
       <div className="flex w-full">
         <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
@@ -74,4 +92,4 @@ const premium = () => {
   );
 };
 
-export default premium;
+export default Premium;
--- tasks.md
+++ tasks.md
@@ -23,9 +23,13 @@ Note : STEP2: In frontend whenever we make any API call - we have to pass in (i.
 - Navbar should update as soon as user logs in.
 
 ## Razorpay payment Gateway Integration 
+       PART 1:
        - Created a UI for premium page 
        - Creating an API for create order in backend
+       - make the API dynamic
        Note : Revision (- 1 h : 22m)
+       PART 2: 
+       - setup Razorpay webhooks on the LIVE api
 
 
 
