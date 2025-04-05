9770903d7517ca9522eff66921766e7901f724b5
amitkumar2k04 <amitkumar2k00@gmail.com>
2025-04-05 23:13:39 +0530

    Added feature re-direct to login if user is not authenticated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,45 @@
-import { Outlet } from "react-router"
-import NavBar from "./NavBar"
-import Footer from "./Footer"
+import { Outlet, useNavigate } from "react-router";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constaints";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const user = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(user.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
-    <div>
+    <>
+      <div>
         <NavBar />
         <Outlet />
         <Footer />
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Body
+export default Body;
