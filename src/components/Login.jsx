27ebb650e9fdbfffda5fcc03f455068645aa17d2
amitkumar2k04 <amitkumar2k00@gmail.com>
2025-04-13 17:32:05 +0530

    Added dynamic signIn/signup feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,11 @@ import { useNavigate } from "react-router";
 import { BASE_URL } from "../utils/constaints";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("deepika123@gmail.com");
-  const [password, setPassword] = useState("Deepika@123");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isLoginFrom, setIsLoginForm] = useState(true);
   const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -22,13 +25,29 @@ const Login = () => {
         },
         { withCredentials: true }
       );
-      console.log(res.data);
+      // console.log(res.data);
       dispatch(addUser(res.data));
-      navigate("/feed");
+      return navigate("/feed");
     } catch (err) {
       setError(err?.response?.data || "Something went wrong");
     }
   };
+
+  const handleSignUp = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        { firstName, lastName, emailId, password },
+        { withCredentials: true }
+      );
+      // console.log(res.data.data);
+      dispatch(addUser(res.data.data));
+      return navigate("/profile");
+    } catch (err) {
+      setError(err?.response?.data || "Something went wrong");
+    }
+  };
+
   return (
     <>
       {/* =================================================================== */}
@@ -58,7 +77,37 @@ const Login = () => {
       <div className="flex justify-center pt-4 pb-12">
         <div className="card bg-neutral w-96 shadow-xl">
           <div className="card-body">
-            <h2 className="card-title justify-center">Login Here!</h2>
+            <h2 className="card-title justify-center">
+              {isLoginFrom ? "Login Here!" : "signUp"}
+            </h2>
+
+            {!isLoginFrom && (
+              <>
+                <label className="form-control w-full max-w-xs">
+                  <div className="label">
+                    <span className="label-text">First Name</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={firstName}
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </label>
+                <label className="form-control w-full max-w-xs">
+                  <div className="label">
+                    <span className="label-text">Last Name</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={lastName}
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </label>
+              </>
+            )}
+
             <label className="form-control w-full max-w-xs">
               <div className="label">
                 <span className="label-text">Email ID</span>
@@ -87,12 +136,18 @@ const Login = () => {
               Forget Password?
             </p>
             <div className="card-actions justify-center mt-4">
-              <button className="btn btn-primary" onClick={handleLogin}>
-                Login
+              <button
+                className="btn btn-primary"
+                onClick={isLoginFrom ? handleLogin : handleSignUp}
+              >
+                {isLoginFrom ? "Login" : "Sign Up"}
               </button>
             </div>
-            <p className="m-auto cursor-pointer text-cyan-400 pt-1 underline">
-              New User? Signup
+            <p
+              className="m-auto cursor-pointer text-cyan-400 pt-1 underline"
+              onClick={() => setIsLoginForm((value) => !value)}
+            >
+              {isLoginFrom ? "New User? Signup" : "Existing User? Login"}
             </p>
           </div>
         </div>
